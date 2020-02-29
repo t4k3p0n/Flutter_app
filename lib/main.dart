e89c0898b05d09e2ec03fd4ee061c595b4043a70
takepon <tqr1104@yahoo.co.jp>
2020-02-29 22:16:16 +0900

    いいねアイコンをタップ可能に

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,15 @@ class RandomWordsState extends State<RandomWords> {
         alreadySaved ? Icons.favorite : Icons.favorite_border,
         color: alreadySaved ? Colors.red : null,
       ),                // この行まで追加
+      onTap: () {      //ここから9行追加します.
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
   // #enddocregion _buildRow
